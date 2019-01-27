ee09ea00e656359a54f0d3869440c7f9718ebec7
Pierre <pgadea@users.noreply.github.com>
2019-01-27 11:07:47 -0600

    updated text to display

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     });
 
     var typed = new Typed(".typed", {
-        strings: ["Web Developer.", "Programmer.", "Student."],
+        strings: ["Web Developer", "Programmer"],
         typeSpeed: 70,
         loop: true,
         startDelay: 1000,
